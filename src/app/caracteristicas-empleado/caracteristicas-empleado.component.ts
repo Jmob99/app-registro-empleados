2190f6d9b08c874bfcbfc06dd75024a22f5b73c7
José Ordóñez <josemiguelordonez99@gmail.com>
2023-06-09 22:53:11 -0500

    se añaden archivos de componentes generados

---
+++ src/app/caracteristicas-empleado/caracteristicas-empleado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caracteristicas-empleado',
+  templateUrl: './caracteristicas-empleado.component.html',
+  styleUrls: ['./caracteristicas-empleado.component.css']
+})
+export class CaracteristicasEmpleadoComponent {
+
+}
---
+++ src/app/hijo/hijo.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Empleado } from '../empleado.component';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.css']
+})
+export class HijoComponent {
+
+  @Input() empleadosEnlace:Empleado;
+  @Input() indice:number;
+}
