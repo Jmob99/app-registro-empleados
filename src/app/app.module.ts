79289f597ce10aec404113cafe8b9074ee542a1c
José Ordóñez <josemiguelordonez99@gmail.com>
2023-06-22 00:31:57 -0500

    se añade navbar y routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,16 +7,34 @@ import { HijoComponent } from './hijo/hijo.component';
 import { CaracteristicasEmpleadoComponent } from './caracteristicas-empleado/caracteristicas-empleado.component';
 import { ServicioEmpleadosService } from './servicio-empleados.service';
 import { DatosEmpleadoService } from './datos-empleado.service';
+import { HomeComponent } from './home/home.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { Routes,RouterModule } from '@angular/router';
+
+
+const appRoutes:Routes = [
+  {path:'', component:HomeComponent},
+  {path:'proyectos', component:ProyectosComponent},
+  {path:'quienes', component:QuienesSomosComponent},
+  {path:'contacto', component:ContactoComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HijoComponent,
-    CaracteristicasEmpleadoComponent
+    CaracteristicasEmpleadoComponent,
+    HomeComponent,
+    ProyectosComponent,
+    QuienesSomosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServicioEmpleadosService, DatosEmpleadoService],
   bootstrap: [AppComponent]
