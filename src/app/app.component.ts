953c9d1283dd47cb6b763d69eaa55c814954745f
José Ordóñez <josemiguelordonez99@gmail.com>
2023-06-23 20:19:23 -0500

    se añaden las rutas de los componentes y se modifica Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Empleado } from './empleado.component';
-import { ServicioEmpleadosService } from './servicio-empleados.service';
-import { DatosEmpleadoService } from './datos-empleado.service';
 
 
 @Component({
@@ -10,32 +7,7 @@ import { DatosEmpleadoService } from './datos-empleado.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Lista de empleados';
-
-  constructor(private miServicio:ServicioEmpleadosService, private datos:DatosEmpleadoService){
-
-    this.array= this.datos.array;
-  }
-
-  array:Empleado[]=[];
-
-  cuadroNombre:string="";
-  cuadroApellido:string="";
-  cuadroCargo:string="";
-  cuadroSalario:number=0;
-
-  enviar(){
-    
-    let miEmpleado= new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
-    this.miServicio.muestraMensaje("Se agrega un nuevo empleado " + miEmpleado.nombre);
-    this.datos.agregarDatos(miEmpleado);
-
-  }
-
-  retirar(){
-    this.array.pop();
-  }
-
+ 
 
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Empleado } from '../empleado.component';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { DatosEmpleadoService } from '../datos-empleado.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  titulo = 'Lista de empleados';
+
+  constructor(private miServicio:ServicioEmpleadosService, private datos:DatosEmpleadoService){
+
+    this.array= this.datos.array;
+  }
+
+
+  array:Empleado[]=[];
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+  enviar(){
+    
+    let miEmpleado= new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    this.miServicio.muestraMensaje("Se agrega un nuevo empleado " + miEmpleado.nombre);
+    this.datos.agregarDatos(miEmpleado);
+
+  }
+
+  retirar(){
+    this.array.pop();
+  }
+
+
 
 }
