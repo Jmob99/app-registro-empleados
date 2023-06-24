58b3ad236d565203e3946eeb71dca6d45dd44a72
José Ordóñez <josemiguelordonez99@gmail.com>
2023-06-23 22:20:48 -0500

    se añade ruta de boton en proyectos

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado.component';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { DatosEmpleadoService } from '../datos-empleado.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -7,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class ProyectosComponent {
 
+  constructor(private router:Router, private miServicio:ServicioEmpleadosService, private datos:DatosEmpleadoService){}
+  array:Empleado[]=[];
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+  enviar(){
+    
+      let miEmpleado= new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+      this.miServicio.muestraMensaje("Se agrega un nuevo empleado " + miEmpleado.nombre);
+      this.datos.agregarDatos(miEmpleado);
+
+    }
+
+  retirar(){
+    this.array.pop();
+    }
+
+  goHome(){
+
+    this.router.navigate([""]);
+  
+  }
+  
 }
